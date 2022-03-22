aba7df4f4ac49c21c03eccd99f1f64312c3ce1e7
Lucas <lucas.ogrodnik@epitech.eu>
2022-03-22 16:57:06 +0100

    Fixing SoloQ

--- src/controllers/summonerController.tsx
+++ src/controllers/summonerController.tsx
@@ -6,14 +6,25 @@ import ISummonerData from '../models/ISummonerData';
 function Summoner(props: any) {
     const [summoner, setSummoner] = useState<ISummonerData>();
 
+    function getSoloQ(data: any) {
+        let goodQueue: any;
+        data.forEach((queue:any) => {
+            if (queue.queueType === "RANKED_SOLO_5x5")
+                goodQueue = queue;
+        });
+        return goodQueue;
+    }
+
     useEffect(() => {
+        let summonerData: any;
         summonerService.getSummonerId(props.name).then(accountResponse => {
             //console.log(accountResponse.data)
             summonerService.getSummonerInfo(accountResponse.data.id).then(statResponse => {
-                statResponse.data[1].icon = accountResponse.data.profileIconId;
-                statResponse.data[1].playerLvl = accountResponse.data.summonerLevel;
-                //console.log(statResponse.data);
-                setSummoner(statResponse.data[1]);
+                console.log(statResponse.data);
+                summonerData = getSoloQ(statResponse.data);
+                summonerData.icon = accountResponse.data.profileIconId;
+                summonerData.playerLvl = accountResponse.data.summonerLevel;
+                setSummoner(summonerData);
             })
         })
     }, []);
